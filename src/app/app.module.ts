37f24e067a17d7f6689e064793009e7a368482e7
Eduardo da Silva Araújo <eduardodevfull@gmail.com>
2021-03-04 15:21:53 -0300

    Pagina de SignIn e de SignUp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainPageModule } from './main-page/main-page.module';
+import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
